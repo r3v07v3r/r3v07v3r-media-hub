a33325c7f3a16091c0f17cdce6194d16256f01ab
Graham <r3v07v3r@users.noreply.github.com>
2026-08-28 16:07:17 +0200

    Generalize anime library page for all media categories

--- src/renderer/src/routes/MoviesPage.tsx
+++ src/renderer/src/routes/MoviesPage.tsx
@@ -1,6 +1,6 @@
-import { CategoryPage } from '@renderer/components/category/CategoryPage'
+import { LibraryPage } from '@renderer/components/category/AnimeLibraryPage'
 import { MOVIES_CONFIG } from '@renderer/lib/mediaHub/categoryConfig'
 
 export default function MoviesPage() {
-  return <CategoryPage config={MOVIES_CONFIG} />
+  return <LibraryPage config={MOVIES_CONFIG} />
 }
